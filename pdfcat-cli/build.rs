97e8241117d528ec4c62b72a78f1aaa05b0f143d
Nelson Dominguez <ekkolon@proton.me>
2025-11-10 14:54:12 +0100

    chore(cli): resolve cargo clippy warnings

--- pdfcat-cli/build.rs
+++ pdfcat-cli/build.rs
@@ -4,9 +4,8 @@ use clap::CommandFactory;
 mod cli;
 
 fn main() -> std::io::Result<()> {
-    let out_dir = std::path::PathBuf::from(
-        std::env::var_os("OUT_DIR").ok_or_else(|| std::io::ErrorKind::NotFound)?,
-    );
+    let out_dir =
+        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
     let cmd = cli::Cli::command();
 
     let man = clap_mangen::Man::new(cmd);
